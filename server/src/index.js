e3f7f9f14dc08b61856587b9bd4e96c3d9254432
sahilrm794 <smustilwar@gmail.com>
2025-12-28 21:41:54 +0530

    set up server with Express, CORS, and MongoDB connection and done with the set up in utils

---
+++ server/src/index.js
@@ -0,0 +1,23 @@
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+import { app } from "./app.js";
+
+dotenv.config({
+    path: './env'
+})
+
+
+
+connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+            console.log("Error :",error);
+            throw error;
+    })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is listening on ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MONGODB connection failed !!",err);
+})
